921d67b6255d79519304a89047596b5e2c0e02fd
Yeming Huang <yeminghuang.lakeforest@gmail.com>
2026-03-15 19:29:11 +0000

    Add wrapper batch files for Task Scheduler

---
+++ ocmonitor/ngrok_wrapper.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\Anson\IdeaProjects\ocmonitor-share\ocmonitor\"
+ngrok http 9394
---
+++ ocmonitor/web_ui_wrapper.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\Anson\IdeaProjects\ocmonitor-share\ocmonitor\"
+ocmonitor web --port 9394 --no-browser
